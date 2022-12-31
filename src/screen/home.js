6d12029d18d20f3d2bbd8a9707762da175836ff0
Samah Alsatary <103362392+alsatarysamah@users.noreply.github.com>
2022-12-31 14:53:18 +0300

    Update home.js

--- src/screen/home.js
+++ src/screen/home.js
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <>
     <Helmet>
-      <title>Amazon</title>
+      <title>T-shirtee</title>
     </Helmet>
       <h1>list of product</h1>
       <Row>
